a780425113c71c3e1cb16d5077570e79a629d621
Ramgopalhyndavbollepalli <rbollep1@uncc.edu>
2024-04-11 21:57:04 -0400

    first commit

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,91 @@
+import styles from "../Styles/landing.module.css";
+
+function Footer() {
+  return (
+    <>
+      <footer
+        style={{
+          paddingTop: "15px",
+          backgroundColor: "#222222",
+          color: "white",
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-3 col-lg-4 col-md-6">
+              <div>
+                <h3>
+                  <span style={{ color: "#ffff00" }}>Bus</span>Booking
+                </h3>
+                <p className="mb-90 footer-desc">
+                Discover, book, and manage bus tickets effortlessly with our user-friendly platform. Search buses, explore routes, view bus details, and book tickets securely. Access your booking history and manage reservations hassle-free. Start your journey with ease!
+
+
+                </p>
+              </div>
+            </div>
+            {/* <div className="col-xl-2 offset-xl-1 col-lg-2 col-md-6">
+              <div className="">
+                <h4>Book</h4>
+                <ul className="list-unstyled">
+                  <li>
+                    <p className="text-decoration-none">Bus Ticket</p>
+                  </li>
+                  <li>
+                    <p className="text-decoration-none">Bus hire</p>
+                  </li>
+                  <li>
+                    <p className="text-decoration-none">Tempo Travellers</p>
+                  </li>
+                  <li>
+                    <p className="text-decoration-none">Car Rentals</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-6">
+              <div>
+                <h4>About</h4>
+                <ul className="list-unstyled">
+                  <li>
+                    <p className="text-decoration-none">About us</p>
+                  </li>
+                  <li>
+                    <p className="text-decoration-none">Contact us</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-6">
+              <div>
+                <h4>Info</h4>
+                <ul className="list-unstyled">
+                  <li>
+                    <p>T & C</p>
+                  </li>
+                  <li>
+                    <p>
+                      <p>Privacy Policy</p>
+                    </p>
+                  </li>
+                  <li>
+                    <p>Cookie Policy</p>
+                  </li>
+                  <li>
+                    <p>FAQ</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center"> */}
+            <div className="copyright">
+                @2024  All rights reserved.            
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
