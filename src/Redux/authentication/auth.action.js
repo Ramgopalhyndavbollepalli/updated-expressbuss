dca75baed2b6124a20664595f613db2477e39714
Ramgopalhyndavbollepalli <rbollep1@uncc.edu>
2024-04-13 19:14:41 -0400

    modified

--- src/Redux/authentication/auth.action.js
+++ src/Redux/authentication/auth.action.js
@@ -9,7 +9,8 @@ import { error, success } from "../../Utils/notification";
 
 export const loginAPI = (data, navigate) => async (dispatch) => {
   try {
-    let response = await axios.post("http://localhost:8080/user/login", data);
+    
+    let response = await axios.post("http://44.217.43.4:8080/user/login", data);
     // console.log(response);
     if (response.data.status === "Failed") {
       error(response.data.message);
